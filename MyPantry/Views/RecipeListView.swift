9f621b1defa0ce3e313f09018c8dd35dc6afd6fb
isha-singhal1 <isingha@purdue.edu>
2025-05-02 12:47:14 -0400

    adding sorting recipes on list view

--- MyPantry/Views/RecipeListView.swift
+++ MyPantry/Views/RecipeListView.swift
@@ -7,26 +7,59 @@
 
 import SwiftUI
 
+enum SortOption: String, CaseIterable, Identifiable {
+    case name = "Name"
+    case prep = "Prep Time"
+    case cook = "Cook Time"
+
+    var id: String { rawValue }
+}
+
 struct RecipeListView: View {
     @StateObject var viewModel = RecipeViewModel()
     @State private var showAddRecipe = false
     @State private var showManageCats = false
     @State private var showReport = false
+    @State private var sortOption: SortOption = .name
 
     var body: some View {
+        let sortedRecipes: [Recipe] = {
+            switch sortOption {
+            case .name:
+                return viewModel.recipes
+                    .sorted { $0.name.lowercased() < $1.name.lowercased() }
+            case .prep:
+                return viewModel.recipes
+                    .sorted { $0.prepTime < $1.prepTime }
+            case .cook:
+                return viewModel.recipes
+                    .sorted { $0.cookTime < $1.cookTime }
+            }
+        }()
         NavigationView {
-            List {
-                ForEach(viewModel.recipes) { recipe in
-                    NavigationLink(destination: RecipeDetailView(recipe: recipe, viewModel: viewModel)) {
-                        Text(recipe.name)
+            VStack(spacing: 8) {
+                // 2️⃣ Sort Picker only
+                Picker("Sort by", selection: $sortOption) {
+                    ForEach(SortOption.allCases) { option in
+                        Text(option.rawValue).tag(option)
                     }
                 }
-                .onDelete { indexSet in
-                    indexSet.forEach { index in
-                        let recipe = viewModel.recipes[index]
-                        viewModel.deleteRecipe(recipe)
+                .pickerStyle(.segmented)
+                .padding([.horizontal, .top])
+                List {
+                    ForEach(sortedRecipes) { recipe in
+                        NavigationLink(destination: RecipeDetailView(recipe: recipe, viewModel: viewModel)) {
+                            Text(recipe.name)
+                        }
+                    }
+                    .onDelete { indexSet in
+                        indexSet.forEach { index in
+                            let recipe = sortedRecipes[index]
+                            viewModel.deleteRecipe(recipe)
+                        }
                     }
                 }
+                .padding(.top, 4)
             }
             .navigationTitle("Recipes")
             .toolbar {
